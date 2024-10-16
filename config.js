1856f58355412a70ed206fc854af7c8fbadbbbc4
pathum2222 <Pathumdesham18@gmail.com>
2024-10-17 00:01:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,9 +5,9 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "put sesson id",
+SESSION_ID: process.env.SESSION_ID || "beR02YQY#uPI3PmJGTJXwE6MSGEdc6OnbfKkvURXAQclbCAhjaT0",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/357b75726739a8b9c68de.jpg",
-SUDO_NB: process.env.SUDO_NB || "ඔයාගෙ owner number එක දෙන්න",
+SUDO_NB: process.env.SUDO_NB || "94717381136",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: "true"
